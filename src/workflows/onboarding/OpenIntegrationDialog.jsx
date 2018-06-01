515bb0abc5fc32385e0193447d4df9ad100760a1
Jack L <jack.leng@gmail.com>
2018-06-01 00:51:27 -0700

    somewhat modular openintegrationdialog / portal field for careprovider mocklist

--- src/workflows/onboarding/OpenIntegrationDialog.jsx
+++ src/workflows/onboarding/OpenIntegrationDialog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {observable, action} from 'mobx'
 import {observer} from 'mobx-react'
+import {find} from 'lodash'
 
 import styles from './OpenIntegrationDialog.module.css'
 import Button from '../../components/Button'
@@ -12,21 +13,20 @@ import mockEntryLists from '../../mockdata/mockEntryLists'
 export default class OpenIntegrationDialog extends React.Component{
 
     render(){
-        console.log('integrateWith', this.props.integrateWith)
-        console.log('of type', this.props.type)
-
+        const app = find(mockEntryLists[this.props.type], (o)=> { return o.name === this.props.integrateWith})
+        const portal = app.portal || app.name
         return(
             <div className = {styles.openIntegrationDialog}>
                 <div className = {styles.image}>
                     <div className = {styles.appBadge}>
-                        <Icon img = "ucsf" size = "large" />
+                        <Icon img = {app.logo} size = "large" />
                     </div>
                 </div>
                 <p className = {styles.context}>
-                    {this.props.app} will ask you for permission to authorize PHIX to use your data now -- make sure to say yes. 
+                    {portal} will ask you for permission to authorize PHIX to use your data now -- make sure to say yes. 
                 </p>
                 <Button 
-                    label = {`OK, go to ${this.props.app}`} 
+                    label = {`OK, go to ${portal}`} 
                     onclick = {this.props.onConfirm}
                 />
             </div>
